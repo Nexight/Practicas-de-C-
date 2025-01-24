f071d0777a8e730d3b66c56b0b220bf0451013b8
NicolasQuinterosgit <nicobquinteros@gmail.com>
2025-01-24 15:53:05 -0300

    proyectos de while y do-shile
    3 proyectos:
    1° código que valida la entrada de enteros
    2°código que valida la entrada de cadena
    3°código que procesa el contenido de una matriz de cadenas

--- CsharpProjects/ChallengesMicrosoft/ProgramCh.cs
+++ CsharpProjects/ChallengesMicrosoft/ProgramCh.cs
@@ -177,7 +177,7 @@ Console.WriteLine($"'o' appears {letterCount} times.");
 //     {
 //         Console.WriteLine(i);
 //     }
-    
+
 // }
 
 #endregion
@@ -195,21 +195,21 @@ Console.WriteLine($"'o' appears {letterCount} times.");
     * Continúe con esta secuencia de ataque hasta que la salud del monstruo o del héroe sea cero o menos.
     * Imprima el ganador.
 */
-Personaje Nico = new Personaje("Nicolás",10,0);
-Personaje monstruo = new Personaje ("Manticora",10,0);
-do
-{
-    Nico.Atacar();
-    monstruo.RecibirDaño(Nico.Ataque);
-    monstruo.VidaCheck();
+// Personaje Nico = new Personaje("Nicolás",10,0);
+// Personaje monstruo = new Personaje ("Manticora",10,0);
+// do
+// {
+//     Nico.Atacar();
+//     monstruo.RecibirDaño(Nico.Ataque);
+//     monstruo.VidaCheck();
 
-    if (monstruo.Vida <= 0) {continue;}
-    
-    monstruo.Atacar();
-    Nico.RecibirDaño(monstruo.Ataque);
-    Nico.VidaCheck();
+//     if (monstruo.Vida <= 0) {continue;}
 
-} while (Nico.Vida > 0 && monstruo.Vida > 0);
+//     monstruo.Atacar();
+//     Nico.RecibirDaño(monstruo.Ataque);
+//     Nico.VidaCheck();
+
+// } while (Nico.Vida > 0 && monstruo.Vida > 0);
 
 // if(Nico.Vida>0)
 //     Console.WriteLine($"{Nico.Nombre} ha triunfado sobre {monstruo.Nombre}!");
@@ -218,34 +218,178 @@ do
 
 //     Console.WriteLine($"El monstruo {monstruo.Nombre} ha derrotado a {Nico.Nombre}!");
 
-Console.WriteLine( Nico.Vida > monstruo.Vida ? $"{Nico.Nombre} ha derrotado a {monstruo.Nombre}!!": $"El enemigo {monstruo.Nombre} ha derrotado a {Nico.Nombre}!");
-public class Personaje
+// Console.WriteLine( Nico.Vida > monstruo.Vida ? $"{Nico.Nombre} ha derrotado a {monstruo.Nombre}!!": $"El enemigo {monstruo.Nombre} ha derrotado a {Nico.Nombre}!");
+// public class Personaje
+// {
+//     public string Nombre {get ; private set;}
+//     Random random = new Random();
+//     public int Vida {get ; private set;}
+//     public int Ataque{get ; private set;}
+//     public Personaje (string nombre,int vida, int ataque)
+//     {
+//         Nombre=nombre;
+//         Vida=vida;
+//         Ataque=ataque;
+//     }
+//     public void Atacar()
+//     {
+//         Ataque = random.Next(1,10);
+//     }
+//     public void RecibirDaño(int daño)
+//     {
+//         Vida -= daño;
+//         Console.WriteLine($"{Nombre} ha perdido {daño} de vida!");
+//     }
+//     public void VidaCheck()
+//     {
+//         if(Vida < 0)
+//             Vida=0;
+//         Console.WriteLine($"La vida actual de {Nombre} es : {Vida}. ");
+//     }
+// }
+#endregion
+#region while, do-while projects.
+
+/*                                                          Proyecto 1: escritura de código que valida la entradad de enteros 
+
+Estas son las condiciones que el primer proyecto de codificación debe implementar:
+
+    La solución debe incluir una iteración do-while o while.
+
+    Antes del bloque de iteración: la solución debe usar una instrucción Console.WriteLine() para solicitar al usuario un valor de enteros entre 5 y 10.
+
+    Dentro del bloque de iteración:
+
+        La solución debe usar una instrucción Console.ReadLine() para obtener la entrada del usuario.
+        La solución debe asegurarse de que la entrada es una representación válida de un entero.
+        Si el valor del entero no está comprendido entre 5 y 10, el código debe usar una instrucción Console.WriteLine() para solicitar al usuario un valor del entero entre 5 y 10.
+        La solución debe garantizar que el valor de enteros está comprendido entre 5 y 10 antes de salir de la iteración.
+        Debajo (después) del bloque de código de iteración, la solución debe usar una instrucción Console.WriteLine() para informar al usuario de que se ha aceptado su valor de entrada.
+*/
+/*
+string? valorEscrito;
+int valorNumerico = 0;
+
+bool esEntero = false;
+bool numeroValido = false;
+
+Console.WriteLine($"Por favor ingrese un numero entero entre 5 y 10.");
+
+do
 {
-    public string Nombre {get ; private set;}
-    Random random = new Random();
-    public int Vida {get ; private set;}
-    public int Ataque{get ; private set;}
-    public Personaje (string nombre,int vida, int ataque)
+    valorEscrito = Console.ReadLine();
+    
+    esEntero = int.TryParse(valorEscrito, out valorNumerico); 
+    if(esEntero)
     {
-        Nombre=nombre;
-        Vida=vida;
-        Ataque=ataque;
-    }
-    public void Atacar()
+      if(valorNumerico>=5 && valorNumerico<=10)
+      {
+        numeroValido=true;
+      }else 
+        Console.WriteLine("El numero ingresado no se encuentra dentro del rango valido. Intentelo nuevamente.");
+
+    }else
+        Console.WriteLine($"El valor ingresado no es valido, intentelo nuevamente.");
+
+}
+while(!numeroValido);
+
+Console.WriteLine($"El número ingresado ({valorNumerico}) ha sido aceptado.");
+*/
+/*            --------------------------------------------------------------------------------------------------------------------------------------------------------------------     
+                                                             Proyecto de código 2: escritura de código que valida la entrada de cadena
+
+Estas son las condiciones que el segundo proyecto de codificación debe implementar:
+
+    La solución debe incluir una iteración do-while o while.
+
+    Antes del bloque de iteración, la solución debe usar una instrucción Console.WriteLine() para solicitar al usuario uno de los tres nombres de rol: Administrador, Director o Usuario.
+
+    Dentro del bloque de iteración:
+
+        La solución debe usar una instrucción Console.ReadLine() para obtener la entrada del usuario.
+        La solución debe garantizar que el valor especificado coincide con una de las tres opciones de rol.
+        La solución debe usar el método Trim() en el valor de entrada para omitir los caracteres de espacio iniciales y finales.
+        La solución debe usar el método ToLower() en el valor de entrada para omitir el caso.
+        Si el valor especificado no coincide con una de las opciones de rol, el código debe usar una instrucción Console.WriteLine() para solicitar al usuario una entrada válida.
+        Debajo (después) del bloque de código de iteración, la solución debe usar una instrucción Console.WriteLine() para informar al usuario de que se ha aceptado su valor de entrada.
+
+*/
+
+// string? rolIngresado ="";
+// string [] roles = {"administrador","director","usuario"};
+
+// bool permisoValido = false;
+
+// Console.WriteLine($"Ingrese su rol.(Administrador,Manager, o Usuario)");
+// while(!permisoValido)
+// {
+//     rolIngresado = Console.ReadLine();
+//     if(rolIngresado != null)
+//     {
+//         foreach (string rol in roles)
+//         {
+//             if(rolIngresado.Trim().ToLower() == rol)
+//             {
+//                 Console.WriteLine($"Rol ingresado correctamente.");
+//                 permisoValido = true;
+//                 break;
+//             }
+//         }
+//         Console.WriteLine($"El rol ingresado no tiene acceso.");
+//     }else 
+//         Console.WriteLine("Por favor ingrese un rol valido.");
+// }
+// Console.WriteLine($"Bienvenido {rolIngresado}.");
+/*            --------------------------------------------------------------------------------------------------------------------------------------------------------------------   
+                                                                Proyecto de código 3: escritura de código que procesa el contenido de una matriz de cadenas
+                                                
+La solución debe usar la siguiente matriz de cadenas para representar la entrada en la lógica de codificación:   
+
+    string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+
+La solución debe declarar una variable de enteros denominada periodLocation que se pueda usar para contener la ubicación del carácter de punto dentro de una cadena.
+
+La solución debe incluir un bucle externo foreach o for que se pueda usar para procesar cada elemento de cadena de la matriz. La variable de cadena que procesará dentro de los bucles debe denominarse myString.
+
+En el bucle externo, la solución debe usar el método IndexOf() de la clase String para obtener la ubicación del primer carácter de punto de la variable myString. La llamada al método debe ser similar a myString.IndexOf("."). Si no hay ningún carácter de punto en la cadena, se devolverá un valor de -1.
+
+La solución debe incluir un bucle interno do-while o while que se pueda usar para procesar la variable myString.
+
+En el bucle interno, la solución debe extraer y mostrar (escribir en la consola) cada frase contenida en cada una de las cadenas que se procesan.
+
+En el bucle interno, la solución no debe mostrar el carácter de punto.
+
+En el bucle interno, la solución debe usar los métodos Remove(), Substring() y TrimStart() para procesar la información de cadena.
+*/
+
+string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+
+int periodLocation = 0;
+
+string myString ="";
+
+foreach(string sentence in myStrings)
+{
+    myString = sentence;
+    periodLocation = myString.IndexOf(".");
+
+    string mySentence;
+
+    while(periodLocation != -1)
     {
-        Ataque = random.Next(1,10);
+        mySentence = myString.Remove(periodLocation);
+
+        myString = myString.Substring(periodLocation +1);
+
+        myString = myString.TrimStart();
+
+        periodLocation = myString.IndexOf(".");
+
+        Console.WriteLine($"{mySentence}");
     }
-    public void RecibirDaño(int daño)
-    {
-        Vida -= daño;
-        Console.WriteLine($"{Nombre} ha perdido {daño} de vida!");
-    }
-    public void VidaCheck()
-    {
-        if(Vida < 0)
-            Vida=0;
-        Console.WriteLine($"La vida actual de {Nombre} es : {Vida}. ");
-    }
+    mySentence=myString.Trim();
+    Console.WriteLine(mySentence);
 }
 #endregion
 //Console.WriteLine($"Fin.");
--- CsharpProjects/ChallengesMicrosoft/obj/Debug/net7.0/ChallengesMicrosoft.AssemblyInfo.cs
+++ CsharpProjects/ChallengesMicrosoft/obj/Debug/net7.0/ChallengesMicrosoft.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengesMicrosoft")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4171ba7543e65d597cbd8f8f7bafbd29bb0d7edb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17ecb239ce8de48f0820db5795185d2758d7a594")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengesMicrosoft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengesMicrosoft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
