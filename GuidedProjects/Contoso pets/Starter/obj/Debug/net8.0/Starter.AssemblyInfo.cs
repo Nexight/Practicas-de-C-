c78384539b25349af2f7cfd3dc597d79b080ae96
NicolasQuinterosgit <nicobquinteros@gmail.com>
2025-02-10 16:07:57 -0300

    Contoso pets: add pets feature
    physical and personality description, age, nicnkame, ID, species.

--- GuidedProjects/Contoso pets/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ GuidedProjects/Contoso pets/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df70d8851813959050a5b51b5492c5d9924dcb2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08e3c729af05fa1ecd72a1045e83af1897d862a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
